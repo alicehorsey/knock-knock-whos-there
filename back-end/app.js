0d9e74c154e113ecf07fc6aa78c996fadb0c17db
Alice Horsey <alicehorsey@gmail.com>
2021-02-13 18:59:38 +0000

    adds error handling

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const jokeRouter = require("./routes/joke_router");
+const { handleInternalErrors, handleCustomErrors, send404 } = require("./controllers/errors");
 
 app.use(express.json());
 const localPort = 8080;
-
-app.use("/jokes", jokeRouter);
-
 app.listen(localPort, () => {
     console.log(`Joke App Back End listening at http://localhost:${localPort}`);
 })
+
+app.use("/jokes", jokeRouter);
+app.use("/*", send404);
+
+app.use(handleCustomErrors);
+app.use(handleInternalErrors);
+
 
 module.exports = app;
