fe420fb9e0b7944e194d2e8005a4638488489902
Alice Horsey <alicehorsey@gmail.com>
2021-02-13 13:36:17 +0000

    corrects port variable names to make connection work

--- back-end/app.js
+++ back-end/app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const jokeRouter = require("./routes/joke_router");
 
-app.use(express.json)
+app.use(express.json());
 const localPort = 8080;
 
 app.use("/jokes", jokeRouter);
 
-app.listen(port, () => {
-    console.log(`Joke App Back End listening at http://localhost:${localPort}`)
+app.listen(localPort, () => {
+    console.log(`Joke App Back End listening at http://localhost:${localPort}`);
 })
 
 module.exports = app;
