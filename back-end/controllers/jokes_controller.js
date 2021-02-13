ec424c2325e495a6f6a957bfea9cd647737d3aad
Alice Horsey <alicehorsey@gmail.com>
2021-02-13 19:00:27 +0000

    adds get joke by id function and catch blocks

--- back-end/controllers/jokes_controller.js
+++ back-end/controllers/jokes_controller.js
@@ -1,11 +1,19 @@
 const jokeRouter = require("../routes/joke_router");
-const { fetchAllJokes } = require("../models/jokes_model");
+const { fetchAllJokes, fetchJokeById } = require("../models/jokes_model");
 
 const getAllJokes = (req, res, next) => {
-    console.log("in controller")
+
     fetchAllJokes().then((jokes) => {
-        res.status(200).send(jokes)
-    })
+        res.status(200).send(jokes);
+    }).catch(next)
 }
 
-module.exports = { getAllJokes };
+const getJokeById = (req, res, next) => {
+    const joke_id = req.params.jokeId;
+
+    fetchJokeById(joke_id).then((joke) => {
+        res.status(200).send(joke);
+    }).catch(next)
+}
+
+module.exports = { getAllJokes, getJokeById };
