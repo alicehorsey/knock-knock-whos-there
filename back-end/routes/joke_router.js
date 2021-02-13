a6f77f15dff33480fed424ed623b9ef49cbac55d
Alice Horsey <alicehorsey@gmail.com>
2021-02-13 13:37:05 +0000

    makes jokes route work for get all jokes

--- back-end/routes/joke_router.js
+++ back-end/routes/joke_router.js
@@ -1,6 +1,7 @@
 const jokeRouter = require("express").Router();
+const { getAllJokes } = require("../controllers/jokes_controller")
 
 jokeRouter.use("/", getAllJokes)
-jokeRouter.use("/:jokeId}", getJokeById)
+// jokeRouter.use("/:jokeId}", getJokeById)
 
 module.exports = jokeRouter;
