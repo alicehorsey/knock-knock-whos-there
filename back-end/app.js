c04c262be011cda50005514c6f3410860f61a03a
Alice Horsey <alicehorsey@gmail.com>
2021-02-13 13:11:23 +0000

    sets up basic app entry point for express server

---
+++ back-end/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const jokeRouter = require("./routes/joke_router");
+
+app.use(express.json)
+const localPort = 8080;
+
+app.use("/jokes", jokeRouter);
+
+app.listen(port, () => {
+    console.log(`Joke App Back End listening at http://localhost:${localPort}`)
+})
+
+module.exports = app;
