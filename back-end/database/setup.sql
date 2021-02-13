bb09d8cd0066cb0bd458c6e516c29ab4afdc96ff
Alice Horsey <alicehorsey@gmail.com>
2021-02-13 13:09:57 +0000

    changes name of joke table

--- back-end/database/setup.sql
+++ back-end/database/setup.sql
@@ -3,8 +3,8 @@ CREATE DATABASE joke_app;
 
 \c joke_app;
 
-CREATE TABLE jokes (
-    id INT PRIMARY KEY,
+CREATE TABLE jokes_table (
+    id SERIAL PRIMARY KEY,
     type VARCHAR(255) NOT NULL,
     setup VARCHAR(255) NOT NULL,
     punchline VARCHAR(255) NOT NULL
