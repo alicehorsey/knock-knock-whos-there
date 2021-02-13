c14120f9f3f3934d0c60a64f5ee007d9d7440b53
Alice Horsey <alicehorsey@gmail.com>
2021-02-13 14:49:20 +0000

    changes app use to app route and get

--- back-end/routes/joke_router.js
+++ back-end/routes/joke_router.js
@@ -1,7 +1,8 @@
 const jokeRouter = require("express").Router();
 const { getAllJokes } = require("../controllers/jokes_controller")
 
-jokeRouter.use("/", getAllJokes)
+jokeRouter.route("/")
+    .get(getAllJokes)
 // jokeRouter.use("/:jokeId}", getJokeById)
 
 module.exports = jokeRouter;
