d7fa9d08320dacf54ee0895f1ba91b3908d86bd0
Alice Horsey <alicehorsey@gmail.com>
2021-02-14 15:08:58 +0000

    makes app a class component and adds basic state

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Loading } from "./components";
+import { getAllJokes } from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    isLoading: false,
+    totalJokes: 0,
+    showTellJokeButton: true,
+    showPunchlineButton: false,
+    showNextJokeButton: false,
+    currentJoke: {},
+    jokesShown: []
+  }
+
+  componentDidMount = () => {
+    getAllJokes().then((jokes) => {
+      console.log(jokes, "in app.js line 20")
+    })
+  }
+
+
+
+
+  render() {
+    const { isLoading } = this.state;
+
+    if (isLoading) {
+      return (
+        <Loading />
+      )
+    }
+
+    return (
+      <div className="App">
+        <h1>Knock Knock Who's There?</h1>
+
+      </div>
+    );
+  }
 }
 
 export default App;
+
