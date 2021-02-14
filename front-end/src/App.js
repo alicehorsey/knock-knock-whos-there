f60d8ff74f6ea017fad37a4cee33a6995fabb4d7
Alice Horsey <alicehorsey@gmail.com>
2021-02-14 16:41:23 +0000

    adds function to next joke button to show the next random joke

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React, { Component } from 'react';
 import { Loading, Title } from "./components";
-import { getAllJokes } from "./api";
+import { getAllJokes, getJokeById } from "./api";
+
 
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
 
   //this function randomly generates a number between 0 and the maximum number it is given as an argument.
   createRandomNumber = (maxNum) => Math.floor(Math.random() * maxNum + 1);
+
 
   componentDidMount = () => {
     getAllJokes().then((jokes) => {
@@ -35,21 +37,30 @@ class App extends Component {
     this.setState({ isShowingPunchlineButton: false, isShowingNextButton: true })
   }
 
+
   handleNextClick = () => {
-    console.log("next click")
+    const nextJokeId = this.createRandomNumber(this.state.totalJokes);
+
+    getJokeById(nextJokeId).then((nextJoke) => {
+      this.setState({ joke: nextJoke, isShowingNextButton: false, isShowingPunchlineButton: true })
+    })
   }
+
 
   render() {
     const { isLoading, isShowingTellJokeButton, isShowingPunchlineButton, isShowingNextButton, joke } = this.state;
 
     if (isLoading) {
       return (
-        <Loading />
+        <div className="App">
+          <Loading />
+        </div>
       )
     } else if (isShowingTellJokeButton) {
       return (
         <div className="App">
           <Title />
+          <h2>Welcome to Knock Knock Who's There! Your one stop shop for the funniest jokes! Click on the button below to start!</h2>
           <button className="tellJokeButton" onClick={this.handleTellJokeClick}>Tell Me A Joke!</button>
         </div>
       );
