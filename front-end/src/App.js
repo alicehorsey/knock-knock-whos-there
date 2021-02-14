59dd2ec5d5110e9f62d371a8c8d562098277303f
Alice Horsey <alicehorsey@gmail.com>
2021-02-14 16:21:08 +0000

    sets up beginning of app up to getting the next joke

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,44 +1,76 @@
 import './App.css';
 import React, { Component } from 'react';
-import { Loading } from "./components";
+import { Loading, Title } from "./components";
 import { getAllJokes } from "./api";
 
 
 class App extends Component {
   state = {
-    isLoading: false,
+    isLoading: true,
     totalJokes: 0,
-    showTellJokeButton: true,
-    showPunchlineButton: false,
-    showNextJokeButton: false,
-    currentJoke: {},
-    jokesShown: []
+    isShowingTellJokeButton: true,
+    isShowingPunchlineButton: false,
+    isShowingNextButton: false,
+    joke: {}
   }
+
+  //this function randomly generates a number between 0 and the maximum number it is given as an argument.
+  createRandomNumber = (maxNum) => Math.floor(Math.random() * maxNum + 1);
 
   componentDidMount = () => {
     getAllJokes().then((jokes) => {
-      console.log(jokes, "in app.js line 20")
+
+      const numberOfJokes = jokes.length;
+      const firstJokeId = this.createRandomNumber(numberOfJokes);
+
+      this.setState({ totalJokes: numberOfJokes, joke: jokes[firstJokeId], isLoading: false })
     })
   }
 
+  handleTellJokeClick = () => {
+    this.setState({ isShowingTellJokeButton: false, isShowingPunchlineButton: true })
+  }
 
+  handlePunchlineClick = () => {
+    this.setState({ isShowingPunchlineButton: false, isShowingNextButton: true })
+  }
 
+  handleNextClick = () => {
+    console.log("next click")
+  }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, isShowingTellJokeButton, isShowingPunchlineButton, isShowingNextButton, joke } = this.state;
 
     if (isLoading) {
       return (
         <Loading />
       )
+    } else if (isShowingTellJokeButton) {
+      return (
+        <div className="App">
+          <Title />
+          <button className="tellJokeButton" onClick={this.handleTellJokeClick}>Tell Me A Joke!</button>
+        </div>
+      );
+    } else if (isShowingPunchlineButton) {
+      return (
+        <div className="App">
+          <Title />
+          <h2>{joke.setup}</h2>
+          <button className="tellPunchlineButton" onClick={this.handlePunchlineClick}>Reveal punchline!</button>
+        </div>
+      );
+    } else if (isShowingNextButton) {
+      return (
+        <div className="App">
+          <Title />
+          <h2>{joke.setup}</h2>
+          <h2>{joke.punchline}</h2>
+          <button className="tellPunchlineButton" onClick={this.handleNextClick}>Next Joke!</button>
+        </div>
+      );
     }
-
-    return (
-      <div className="App">
-        <h1>Knock Knock Who's There?</h1>
-
-      </div>
-    );
   }
 }
 
