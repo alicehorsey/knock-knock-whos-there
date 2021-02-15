32887bfc45aff3803c8d3dab18ff3cfea375a723
Alice Horsey <alicehorsey@gmail.com>
2021-02-15 14:09:48 +0000

    adds style to Title

--- front-end/src/components/Title.jsx
+++ front-end/src/components/Title.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+const textStyle = {
+    'padding-top': '10vh',
+    'padding-bottom': '2vh',
+    margin: 0,
+    'font-size': '6em',
+    'font-family': 'American Typewriter, serif',
+    'background-image': 'linear-gradient(to left, violet, violet, violet, indigo, blue, green, yellow, orange, red, red, red)',
+    '-webkit-background-clip': 'text',
+    color: 'transparent',
+}
+
+
 const Title = () => {
     return (
-        <div>
-            <h1>Knock Knock Who's There?</h1>
-        </div>
+        <h1 style={textStyle}>Knock Knock Who's There?</h1>
     );
 };
 
