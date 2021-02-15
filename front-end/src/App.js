7d8dcbeaa8f4b4c30656c85a67b90229f4a7c76f
Alice Horsey <alicehorsey@gmail.com>
2021-02-15 11:25:54 +0000

    updates variable names, refactors, adds final refresh functionality

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,10 +9,11 @@ class App extends Component {
   state = {
     isLoading: true,
     totalJokes: 0,
-    isShowingTellJokeButton: true,
-    isShowingPunchlineButton: false,
-    isShowingNextButton: false,
-    joke: {}
+    displayTellJokeButton: true,
+    displayPunchlineButton: false,
+    displayNextButton: false,
+    joke: {},
+    jokesShown: null,
   }
 
   //this function randomly generates a number between 0 and the maximum number it is given as an argument.
@@ -25,30 +26,58 @@ class App extends Component {
       const numberOfJokes = jokes.length;
       const firstJokeId = this.createRandomNumber(numberOfJokes);
 
-      this.setState({ totalJokes: numberOfJokes, joke: jokes[firstJokeId], isLoading: false })
+      this.setState({ totalJokes: numberOfJokes, joke: jokes[firstJokeId - 1], isLoading: false, jokesShown: [firstJokeId] })
     })
   }
 
   handleTellJokeClick = () => {
-    this.setState({ isShowingTellJokeButton: false, isShowingPunchlineButton: true })
+    this.setState({ displayTellJokeButton: false, displayPunchlineButton: true })
   }
 
   handlePunchlineClick = () => {
-    this.setState({ isShowingPunchlineButton: false, isShowingNextButton: true })
+    this.setState({ displayPunchlineButton: false, displayNextButton: true })
   }
 
+  //This function is to check that we don't randomly generate the same joke multiple times.
+  generateNextJokeId = () => {
+    const proposedNextJokeId = this.createRandomNumber(this.state.totalJokes);
 
+    return !this.state.jokesShown.includes(proposedNextJokeId) ? proposedNextJokeId : this.generateNextJokeId();
+  }
+
+  //This function lines up the next joke, making sure that that randomly generated joke id has not been seen before.
   handleNextClick = () => {
-    const nextJokeId = this.createRandomNumber(this.state.totalJokes);
+    const nextJokeId = this.generateNextJokeId();
 
     getJokeById(nextJokeId).then((nextJoke) => {
-      this.setState({ joke: nextJoke, isShowingNextButton: false, isShowingPunchlineButton: true })
+
+      this.setState((prevState) => ({
+        joke: nextJoke,
+        jokesShown: [...prevState.jokesShown, nextJokeId],
+        displayNextButton: false,
+        displayPunchlineButton: true
+      })
+      )
+    })
+  }
+
+  //This function gets any random number and resets the jokesShown array to remove all previously shown jokes to start the app from the beginning again.
+  handleRefreshClick = () => {
+    const refreshedFirstJokeId = this.createRandomNumber(this.state.totalJokes);
+
+    getJokeById(refreshedFirstJokeId).then((nextJoke) => {
+      this.setState({
+        joke: nextJoke,
+        jokesShown: [refreshedFirstJokeId],
+        displayNextButton: false,
+        displayPunchlineButton: true
+      })
     })
   }
 
 
   render() {
-    const { isLoading, isShowingTellJokeButton, isShowingPunchlineButton, isShowingNextButton, joke } = this.state;
+    const { isLoading, displayTellJokeButton, displayPunchlineButton, displayNextButton, joke, totalJokes, jokesShown } = this.state;
 
     if (isLoading) {
       return (
@@ -56,7 +85,7 @@ class App extends Component {
           <Loading />
         </div>
       )
-    } else if (isShowingTellJokeButton) {
+    } else if (displayTellJokeButton) {
       return (
         <div className="App">
           <Title />
@@ -64,7 +93,7 @@ class App extends Component {
           <button className="tellJokeButton" onClick={this.handleTellJokeClick}>Tell Me A Joke!</button>
         </div>
       );
-    } else if (isShowingPunchlineButton) {
+    } else if (displayPunchlineButton) {
       return (
         <div className="App">
           <Title />
@@ -72,13 +101,18 @@ class App extends Component {
           <button className="tellPunchlineButton" onClick={this.handlePunchlineClick}>Reveal punchline!</button>
         </div>
       );
-    } else if (isShowingNextButton) {
+    } else if (displayNextButton) {
+
+      //When we get the end of all the jokes the Next Button changes to say Refresh to start from the beginning again.
+      const isNext = jokesShown.length !== totalJokes;
+
       return (
         <div className="App">
           <Title />
           <h2>{joke.setup}</h2>
           <h2>{joke.punchline}</h2>
-          <button className="tellPunchlineButton" onClick={this.handleNextClick}>Next Joke!</button>
+          <p hidden={isNext}>We're sorry to say this is your last joke! Boooo! Click on the button to refresh!</p>
+          <button className="NextButton" onClick={isNext ? this.handleNextClick : this.handleRefreshClick}>{isNext ? "Next Joke!" : "Refresh"}</button>
         </div>
       );
     }
