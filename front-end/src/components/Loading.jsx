ceae34ea4d9776b02810a519c06f4f905a5b8843
Alice Horsey <alicehorsey@gmail.com>
2021-02-15 14:13:37 +0000

    adds style to loading

--- front-end/src/components/Loading.jsx
+++ front-end/src/components/Loading.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+const loadingStyle = {
+    'padding-top': '10vh',
+    'padding-bottom': '2vh',
+    margin: 0,
+    'font-size': '6em',
+    'font-family': 'American Typewriter, serif',
+    color: 'orange',
+}
+
 const Loading = () => {
     return (
-        <div>
-            <h1>Loading!</h1>
-        </div>
+        <h1 style={loadingStyle}>Loading</h1>
     );
 };
 
